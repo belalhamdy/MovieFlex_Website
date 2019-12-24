171f609708b00d5472447a9bbccbd7907897cbfe
Belal Hamdy <36121094+belalhamdy@users.noreply.github.com>
2019-12-24 13:39:56 +0200

    init

---
+++ Database/Movies_Database_Getter/Movies_Database_Getter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Movies_Database_Getter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
